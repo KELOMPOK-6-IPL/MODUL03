6be4987bc30032d5f5266852aa319a80fbc44e82
thinkerhour <thinkerhour@gmail.com>
2025-11-03 15:57:41 +0700

    menambahkan file motorcycle

---
+++ Motorcycle.java
@@ -0,0 +1,16 @@
+public class Motorcycle {
+    // Can be implemented
+    @Override
+    public void drive() {
+    }
+    @Override
+    public void stop() {
+    }
+    @Override
+    public void refuel() {
+    }
+    // Can not be implemented
+    @Override
+    public void openDoors() {
+    }
+}
